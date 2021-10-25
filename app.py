edef610ebeec8070fb488c91b0c823688e3cb22c
ninabohm <nina.boehm1994@gmail.com>
2021-10-25 13:59:06 +0200

    remove userinput class and merge with encryption class

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-class UserInput:
-
-    def get_userInput_from_cli(self):
-        newInput = input()
-        self.content = newInput
-
-
 class Encryption:
 
     def __init__(self):
         self.alphabetIndex = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
             "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
             "0","1","2","3","4","5","6","7","8","9"]
-        
+    
+
+    def get_userInput_from_cli(self):
+        newInput = input()
+        self.userInput = newInput
+
 
     def encrypt_input(self, userInput):
         #TODO @ Lina :), currently encryptedInput is a placeholder variable
@@ -57,10 +55,9 @@ def runTests():
 
 def main():
     while True: 
-        userInput = UserInput()
-        userInput.get_userInput_from_cli()
         newEncryption = CaesarEncryption()
-        newEncryption.encrypt_input(userInput.content)
+        newEncryption.get_userInput_from_cli()
+        newEncryption.encrypt_input(newEncryption.userInput)
         print(newEncryption.encryptedContent)
 
 
