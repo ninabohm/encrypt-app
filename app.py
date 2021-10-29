ebef6e16fa50ae051960ccfb066209e8a32084de
Lina <Lina.Stockmann@Student.HTW-Berlin.de>
2021-10-29 15:16:54 +0200

    encrypt_input method merge into dev

--- app.py
+++ app.py
@@ -14,18 +14,18 @@ class Encryption:
     def encrypt_input(self, userInput):
         #TODO @ Lina :), currently encryptedInput is a placeholder variable
 
-        input
-        caeserencryption = []
+        caeserencryption = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+            "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+            "0","1","2","3","4","5","6","7","8","9"]
 
-        def caeserencryption (userInput):
-            for x in userInput:
-                if x == " ":
-                    userInput[x] = " "
-                if x == "9":
-
-                else:
-                    y = caeserencryption.index(userInput[x])
-                    userInput[x] = caeserencryption[y+1]
+        for x in userInput:
+            if x == " ":
+                userInput[x] = " "
+            elif x == "9":
+                userInput[x] = caeserencryption[0]
+            else:
+                y = caeserencryption.index(userInput[x])
+                userInput[x] = caeserencryption[y+1]
         
         encryptedInput = userInput + "-encrypted"
         self.encryptedContent = encryptedInput
