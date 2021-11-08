04e2d5dd45e9b0598eff9006d76a27c1eb2852cd
ninabohm <nina.boehm1994@gmail.com>
2021-11-08 13:01:39 +0100

    add option to choose with command promt

--- app.py
+++ app.py
@@ -2,20 +2,27 @@ from encryption.caesar_encryption import CaesarEncryption
 from encryption.monoalphabetic_substitution import MonoalphabeticSubstitution
 
 
-if __name__ == '__main__':
-    print("chosse \"c\" for Caesar Encryption and \"m\" for Monoalphabetic Substitution")
-    encryptionType = input()
-    if encryptionType == "c":
-        while True: 
-            newEncryption = CaesarEncryption()
-            newEncryption.get_userInput_from_cli()
-            newEncryption.encrypt_input(newEncryption.userInput)
-            print(newEncryption.encryptedContent)
-    
-    if encryptionType == "m":
-        while True: 
-            newEncryption = MonoalphabeticSubstitution()
-            newEncryption.get_userInput_from_cli()
-            newEncryption.encrypt_input(newEncryption.userInput)
-            print(newEncryption.encryptedContent)
+class App:
+
+    if __name__ == '__main__':
+        encryptionType = input("Press c for Caesar Encryption and m for Monoalphabetic Substitution: ")
+
+        if encryptionType == "c":
+            print("Caeser Encryption started. Please enter a word to encrypt: ")
+            while True: 
+                newEncryption = CaesarEncryption()
+                newEncryption.get_userInput_from_cli()
+                newEncryption.encrypt_input(newEncryption.userInput)
+                print(newEncryption.encryptedContent)
+        
+        if encryptionType == "m":
+            print("Monoalphabetic Substitution Encryption started. Please enter a word to encrypt: ")
+            while True: 
+                newEncryption = MonoalphabeticSubstitution()
+                newEncryption.get_userInput_from_cli()
+                newEncryption.encrypt_input(newEncryption.userInput)
+                print(newEncryption.encryptedContent)
+        
+        
+            
     
