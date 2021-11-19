12c5783e82789ec2a51da9a829ed8e8c28762f39
ninabohm <nina.boehm1994@gmail.com>
2021-11-19 21:46:24 +0100

    add logs to register

--- user/user.py
+++ user/user.py
@@ -1,16 +1,22 @@
 from user.user_list import user_list
+import logging
 
+
+logger = logging.getLogger(__name__)
 
 class User:
 
     def __init__(self):
         self.logged_in = False
 
+
     def create_user(self):
         self.firstName = input("Please type your first name and press enter ")
 
+
     def save_user(self):
         user_list.append(self.firstName)
+        logger.info(f"User with first name {self.firstName} saved in user list")
 
 
     def check_if_in_user_list(self, user):
