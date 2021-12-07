0d6862f262488517ade5a985ea4742ab92ef2a5c
ninabohm <nina.boehm1994@gmail.com>
2021-12-07 11:50:06 +0100

    add encrypted string tests

---
+++ encryption/encrypted_string.py
@@ -0,0 +1,4 @@
+class EncryptedString:
+
+    def __init__(self, encrypted_contend: str):
+        self.enc_string = encrypted_contend
---
+++ tests/test_encrypted_string.py
@@ -0,0 +1,9 @@
+import unittest
+from encryption.encrypted_string import EncryptedString
+
+
+class TestEncryptedString(unittest.TestCase):
+
+    def test_should_return_bcd_given_encrypted_string_is_bcd(self):
+        sample_string = EncryptedString("bcd")
+        self.assertEqual(sample_string.enc_string, "bcd")
