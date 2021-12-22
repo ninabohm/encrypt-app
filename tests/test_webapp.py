d2d504a249232f4435b12edbf0e3f50a0d715f34
ninabohm <nina.boehm1994@gmail.com>
2021-12-22 10:03:56 +0100

    add result template

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -1,4 +1,5 @@
 import unittest
+from flask import url_for
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import logging
@@ -25,4 +26,18 @@ class TestWebapp(unittest.TestCase):
 
     def test_given_index_browser_title_contains_welcome(self):
         self.assertIn("Welcome", self.driver.title)
+
+    def test_index_has_input_for_login(self):
+        input_element = self.driver.find_element_by_css_selector(f'[id="username"]')
+        self.assertIsNotNone(input_element)
+
+    def test_given_post_to_encryption_endpoint_returns_200(self):
+        with app.test_client() as client:
+            response = client.get("/encryption")
+            assert response._status_code == 200
+
+    def test_given_post_to_result_endpoint_returns_200(self):
+        with app.test_client() as client:
+            response = client.post("/result")
+            assert response._status_code == 200
 
--- webapp.py
+++ webapp.py
@@ -9,5 +9,15 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/encryption", methods=['GET'])
+def encryption():
+    return render_template("encryption.html")
+
+
+@app.route("/result", methods=['POST'])
+def result():
+    return render_template("result.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
