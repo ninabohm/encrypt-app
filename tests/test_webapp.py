f17bcec0f5c2c21f2498bbeae548c566b55b2148
ninabohm <nina.boehm1994@gmail.com>
2021-12-22 09:04:28 +0100

    add login form placeholder

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -1,14 +1,28 @@
 import unittest
-
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import logging
 
 from webapp import app
 
+logger = logging.getLogger(__name__)
+
 
 class TestWebapp(unittest.TestCase):
+
+    def setUp(self):
+        ser = Service("./chromedriver")
+        self.driver = webdriver.Chrome(service=ser)
+        self.driver.get("http://localhost:5000")
+
+    def tearDown(self):
+        self.driver.quit()
 
     def test_given_app_running_index_endpoint_returns_200(self):
         with app.test_client() as client:
             response = client.get("/")
             assert response._status_code == 200
 
+    def test_given_index_browser_title_contains_welcome(self):
+        self.assertIn("Welcome", self.driver.title)
 
--- webapp.py
+++ webapp.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, request, render_template
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "index site"
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
