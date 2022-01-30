948672ace8d6f26ac5035304355cf356512eea27
ninabohm <nina.boehm1994@gmail.com>
2022-01-30 20:22:50 +0100

    some spacing

--- forms.py
+++ forms.py
@@ -3,15 +3,16 @@ from wtforms.validators import DataRequired, Length, NumberRange
 
 
 class RegisterForm(Form):
-    user_name = StringField("Username", [DataRequired()])
-    password = PasswordField("Password (min. 6)", [DataRequired(), Length(min=6)])
+    user_name = StringField("Username", [DataRequired(), Length(max=20)])
+    password = PasswordField("Password (min. 6)", [DataRequired(), Length(min=6, max=30)])
 
 
 class LoginForm(Form):
-    user_name = StringField("Username", [DataRequired()])
-    password = PasswordField("Password", [DataRequired(), Length(min=6)])
+    user_name = StringField("Username", [DataRequired(), Length(max=20)])
+    password = PasswordField("Password", [DataRequired(), Length(min=6, max=30)])
 
 
 class EncryptionForm(Form):
     shift = IntegerField("Shift (1-1024)", [NumberRange(min=1, max=1024, message="Invalid number")])
-    user_input = StringField("Your word to be encrypted", [DataRequired()])
+    user_input = StringField("Your text to be encrypted", [DataRequired(), Length(max=50)])
+
